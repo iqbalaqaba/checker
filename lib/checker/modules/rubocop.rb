9fc9698cbeeaa6019c938a4d8902e03ea9a543b6
Jacek Jakubik <jacek.jakubik@netguru.pl>
2018-01-17 15:28:08 +0100

    Change rubocop check to use multifile

--- lib/checker/modules/rubocop.rb
+++ lib/checker/modules/rubocop.rb
@@ -1,16 +1,25 @@
 module Checker
   module Modules
-    class Rubocop < Base
-      extensions 'rb'
+    class Rubocop < Multifile
       private
-      def check_one(file, opts = {})
-        Checker::Result.result(self, plain_command("rubocop #{file}", :bundler => false))
+      def check_all(files, opts = {})
+        Checker::Result.result(self, plain_command("rubocop -f c #{files.join(" ")}", :bundler => false))
       end
 
       def dependency_message
         str = "Executable not found\n"
         str << "Install rubocop using rubygems: 'gem install rubocop'"
         str
+      end
+
+      def fix_message(files)
+        "\nTo Autocorrect, execute: rubocop -a #{files.join(" ")}\n"
+      end
+
+      def after_check
+        if @results.first == false
+          puts fix_message(files_to_check).colorize(:green)
+        end
       end
     end
   end
